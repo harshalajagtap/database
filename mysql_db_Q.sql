2e2ef89df53440ced7972452841ecdd6f3de2c97
harshala jagtap <harshalajagtap1619@gmail.com>
2022-06-14 20:05:00 +0530

    employee fname as empname in upper case

--- mysql_db_Q.sql
+++ mysql_db_Q.sql
@@ -1,2 +1,4 @@
 select * from empolyee.employee_position;
 select * from empolyee.emp_basic_details;
+/*Q1. Write a query to fetch the EmpFname from the EmployeeInfo table in upper case and use the ALIAS name as EmpName.*/
+select upper(EmpFname) as EmpName from empolyee.emp_basic_details;
