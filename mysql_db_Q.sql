79ee4aa0b168dd7577f52f2e265111f705d7df57
harshala jagtap <harshalajagtap1619@gmail.com>
2022-06-14 20:10:56 +0530

    getting system date and time

--- mysql_db_Q.sql
+++ mysql_db_Q.sql
@@ -3,3 +3,4 @@ select * from empolyee.emp_basic_details;
 /*Q1. Write a query to fetch the EmpFname from the EmployeeInfo table in upper case and use the ALIAS name as EmpName.*/
 select upper(EmpFname) as EmpName from empolyee.emp_basic_details;
 select count(*) from empolyee.emp_basic_details where Department = "HR";
+select sysdate()
