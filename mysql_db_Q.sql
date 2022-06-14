ac7c68ada920d568c3c3f22951838dacdde849de
harshala jagtap <harshalajagtap1619@gmail.com>
2022-06-14 23:26:18 +0530

    using MID and LOCATE

--- mysql_db_Q.sql
+++ mysql_db_Q.sql
@@ -4,4 +4,5 @@ select * from empolyee.emp_basic_details;
 select upper(EmpFname) as EmpName from empolyee.emp_basic_details;
 select count(*) from empolyee.emp_basic_details where Department = "HR";
 select sysdate();
-select substring(EmpLname,1,4) from empolyee.emp_basic_details
+select substring(EmpLname,1,4) from empolyee.emp_basic_details;
+SELECT MID(Address,1,LOCATE('(',Address) )FROM empolyee.emp_basic_details;
