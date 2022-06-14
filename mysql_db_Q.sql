988e30d5b70f33c7d5c266fd2c84ae7dcba2936d
harshala jagtap <harshalajagtap1619@gmail.com>
2022-06-15 00:02:07 +0530

    employee details who having salary between 50000 and 100000

--- mysql_db_Q.sql
+++ mysql_db_Q.sql
@@ -7,3 +7,4 @@ select sysdate();
 select substring(EmpLname,1,4) from empolyee.emp_basic_details;
 SELECT MID(Address,1,LOCATE('(',Address) )FROM empolyee.emp_basic_details;
 create table new_table1 as select * from emp_basic_details;
+SELECT * FROM employee_position WHERE Salary BETWEEN '50000' and '100000';
