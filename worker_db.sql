0d565799fa860b5f8b8775fd2f379b28614c56ae
harshala jagtap <harshalajagtap1619@gmail.com>
2022-06-20 11:35:01 +0530

    mysql practice query

---
+++ worker_db.sql
@@ -0,0 +1,9 @@
+select First_name as worker_name from worker;
+select upper(First_name) from worker; 
+select distinct DEPARTMENT from worker;
+select substring(First_name,1,3) from worker;
+select instr(First_name, Binary 'a') from worker where First_name = "Amitabh";
+select rtrim(First_name) from worker;
+select ltrim(DEPARTMENT) from worker;
+select distinct length(DEPARTMENT) from worker;
+select replace(First_name,'a','A') from worker;
